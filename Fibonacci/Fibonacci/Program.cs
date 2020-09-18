43382e3a77b9a7c02bc75c3041cb4bf1adbea3b2
bryanpozo <bryanmanuelmedinapozo2015@gmail.com>
2020-09-17 22:24:57 -0400

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, numero, i, ayuda;
+
+
+            Console.Write("Ingrese el número de Fibonacci que desea mostrar: ");
+            numero = int.Parse(Console.ReadLine());
+
+            a = 0;
+            b = 1;
+            for (i = 0; i < numero; i++)
+            {
+                ayuda = a;
+                a = b;
+                b = ayuda + a;
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
