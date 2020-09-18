f3b0f296f4ccff80176d2f25b03d6cb62d80f6c9
bryanpozo <bryanmanuelmedinapozo2015@gmail.com>
2020-09-17 22:24:21 -0400

    Add files via upload

---
+++ primo/primo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace primo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entrar el numero que desea comenzar: ");
+            int startNumber = int.Parse(Console.ReadLine());
+            Console.Write("Entrar numero que desea terminar: ");
+
+            int endNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Numeros primos entre {startNumber} y {endNumber} son : ");
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+
+                if (counter == 0 && i != 1)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
